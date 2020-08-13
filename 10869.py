130a54fab14b27eaa20d1557846562f4d1c99044
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2020-08-13 23:47:53 +0900

    Create 10869.py

---
+++ 10869.py
@@ -0,0 +1,15 @@
+"""
+1. exact value of division : /
+2. quotient : //
+3. remainder : %
+"""
+a ,b = input().split()
+
+a = int(a)
+b = int(b)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
