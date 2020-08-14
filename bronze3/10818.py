a2644e57a067ff7abbb6c33f970ae363a906beaa
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2020-08-14 23:53:59 +0900

    Create 10818.py

---
+++ bronze3/10818.py
@@ -0,0 +1,13 @@
+N = int(input())
+max = 0
+min = 1000000
+number = []
+number = input().split()
+
+for i in range(N):
+  if int(number[i]) > max:
+    max = int(number[i])
+  elif int(number[i]) < min:
+    min = int(number[i])
+
+print(min,max)
