fb5b129f10c3a4a2b411fa083f7dbbff49fdd2e5
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2020-08-20 00:21:05 +0900

    Update and rename not16134.py to 16134.py
    핵심 : 팩토리얼 먼저 계산후 modpow

--- gold1/16134.py
+++ gold1/16134.py
@@ -1,12 +1,14 @@
+# 핵심 : 팩토리얼 먼저 계산후 modpow
 def modpow(x, n, mod):
     res = 1;
     while n > 0:
         # 최하위 비트가 서있을 경우에 x ^ (2^i)를 곱한다.
-        if n & 1:
+        if n%2 == 1:
             res = res * x % mod
         x = x * x % mod
         n //= 2
     return res
+    
 
 N, R = map(int,input().split())
 
@@ -16,12 +18,14 @@ if R > (N//2):
 ans = 1
 for i in range(R):
     ans *= N-i
-    if ans >1000000007:
-        ans = ans%1000000007
+    ans %= 1000000007
     # print("i ",ans)
 
+fac = 1
 for j in range(R):
-    ans *= modpow(j+1,1000000005,1000000007)
-    if ans > 1000000007:
-        ans = ans%1000000007
+    fac *= j+1
+    fac %= 1000000007
+
+ans *= modpow(fac,1000000005,1000000007)
+ans %= 1000000007
 print(ans)
