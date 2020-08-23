d4a6ea41f0014e0ee73b8c052913ae8b6508761d
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2020-08-23 17:31:10 +0900

    Update not1929.py

--- silver2/not1929.py
+++ silver2/not1929.py
@@ -5,19 +5,18 @@ prime = [2]
 
 
 for i in range(3,N+1,2):
-    if i != 1:
-        boolean = True
-        length = len(prime)
-        for j in range(length):
-            if i%prime[j] == 0:
-                boolean = False
-                # print('break')
-                break
-            if math.sqrt(i) <= prime[j]:
-                # print('leak')
-                break
-        if boolean:
-            prime.append(i)
+    boolean = True
+    length = len(prime)
+    for j in range(length):
+        if i%prime[j] == 0:
+            boolean = False
+            # print('break')
+            break
+        if math.sqrt(i) <= prime[j]:
+            # print('leak')
+            break
+    if boolean:
+        prime.append(i)
             
 for i in range(M,N+1):
     if i in prime:
