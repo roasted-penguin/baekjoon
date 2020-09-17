cc0d247761f99701c61a35932f21d3897b050558
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2020-09-18 00:02:00 +0900

    Update 11727.py

--- silver3/11727.py
+++ silver3/11727.py
@@ -1,3 +1,4 @@
+#1
 def com(N,K):
   ans = 1
   if K != 0:
@@ -21,3 +22,12 @@ while r >= 0:
   n -= 1
   r -= 2
 print(ans)
+
+#2
+n = int(input())
+
+do = [0,1,3]
+for i in range(3,n+1):
+  do.append((do[i-1] + 2*do[i-2])%10007)
+
+print(do[n])
