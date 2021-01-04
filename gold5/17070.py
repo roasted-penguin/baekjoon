44f359c9321e5c98f52bf058e3a699c61ac7160b
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2021-01-04 23:30:12 +0900

    Update and rename not17070.py to 17070.py

--- gold5/17070.py
+++ gold5/17070.py
@@ -7,38 +7,30 @@ if tile[0][2] == 0:
     direction[0][2] = [1,0,0]
 if [tile[0][2], tile[1][1], tile[1][2]] == [0,0,0]:
     direction[1][2] = [0,1,0]
-
+    
 for i in range(N):
     for j in range(N):
-        print(i,j)
         if direction[i][j][0] > 0:#horizontal
             num = direction[i][j][0]
             if j+1 < N and tile[i][j+1] == 0:#move horizontal
                 direction[i][j+1][0] += num
-                print("h,h")
             if i+1 < N and j+1 < N  and tile[i][j+1] == 0 and tile[i+1][j+1]==0 and tile[i+1][j]==0:#move diagonal
                 direction[i+1][j+1][1] += num
-                print("h,d")
         if direction[i][j][1] > 0:#diagonal
             num = direction[i][j][1]
             if j+1 < N and tile[i][j+1] == 0:#move horizontal
                 direction[i][j+1][0] += num
-                print("d,h")
             if i+1 < N and j+1 < N  and tile[i][j+1]==0 and tile[i+1][j+1]==0 and tile[i+1][j]==0:#move diagonal
                 direction[i+1][j+1][1] += num
-                print("d,d")
             if i+1 < N and tile[i+1][j]==0:#move vertical
                 direction[i+1][j][2] += num
-                print("d,v")
         if direction[i][j][2] > 0:#vertical
             num = direction[i][j][2]
             if i+1 < N and j+1 < N  and tile[i][j+1]==0 and tile[i+1][j+1]==0 and tile[i+1][j]==0:#move diagonal
-                direction[i+1][j+1][0] += num
-                print("v,d")
+                direction[i+1][j+1][1] += num
             if i+1 < N and tile[i+1][j]==0:#move:#move vertical
                 direction[i+1][j][2] += num
-                print("v,v")
 
 ans = direction[N-1][N-1][0] + direction[N-1][N-1][1] + direction[N-1][N-1][2]
 print(ans)
-print(direction)
+#print(direction)
