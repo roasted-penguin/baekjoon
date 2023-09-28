7f71dfd3143834ec3857189afb6966a4244aebba
roasted-penguin <43606521+roasted-penguin@users.noreply.github.com>
2023-09-28 21:14:12 +0900

    Create 15650.py
    조합 : 백트래킹

---
+++ silver3/15650.py
@@ -0,0 +1,18 @@
+N, M = map(int,input().split())
+
+def dfs(n,arr):
+  if len(arr)==M:
+    print_arr(arr)
+    return
+  for i in range(n+1,N+1):
+    arr.append(i)
+    dfs(i,arr)
+    arr.pop()
+  
+
+def print_arr(arr):
+  for item in arr:
+    print(item,end=' ')
+  print()
+  
+dfs(0,[])
